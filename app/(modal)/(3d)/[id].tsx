9cbe60c0fcb9a5eb4360a55d04a89bbe5d0adafb
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-07-31 11:46:44 +0430

    setup 3d picture view

---
+++ app/(modal)/(3d)/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Screen3D = () => {
+  return (
+    <View>
+      <Text>Screen3D</Text>
+    </View>
+  );
+};
+
+export default Screen3D;
